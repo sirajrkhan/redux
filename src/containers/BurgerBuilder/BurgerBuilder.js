c5073ed536b4e153a4a325ac55972d7e1c60e4f1
Siraj Khan <sirajrkhan@live.in>
2020-02-25 16:54:15 +0530

    Modal window added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component{
     purchaseHandler = () => {
         this.setState({purchasing:true})
     }
+    
+    purchaseCancelHandler = () =>{
+        this.setState({purchasing:false})
+    }
 
     render () {
         const disabledInfo = {
@@ -86,7 +90,10 @@ class BurgerBuilder extends Component{
 
         return(
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal 
+                    show={this.state.purchasing} 
+                    modalClosed={this.purchaseCancelHandler}
+                >
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingred={this.state.ingredients} />
