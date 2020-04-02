499368cd50696605170215fa795ce0907a7017f5
Siraj Khan <sirajrkhan@live.in>
2020-04-02 14:01:11 +0530

    Scaffolding for redux completed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './store/reducer';
 import './index.scss';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
-const app = <BrowserRouter> <App /> </BrowserRouter>;
+const store = createStore(reducer);
+const app = <Provider store={store}> <BrowserRouter> <App /> </BrowserRouter> </Provider>;
 
 ReactDOM.render(app, document.getElementById('root'));
 
---
+++ src/store/reducer.js
@@ -0,0 +1,5 @@
+const reducer = (state,action) => {
+    action = null;
+    return state;
+}
+export default reducer;
